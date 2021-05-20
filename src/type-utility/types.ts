49ba6e4f658ab78abf19d0bd5dd257f85f4f1c52
nicole <shchon@lunasoft.co.kr>
2021-05-20 10:22:10 +0900

    fix types.ts

--- src/type-utility/types.ts
+++ src/type-utility/types.ts
@@ -1,6 +1,28 @@
 import { ChatElement } from 'chat-element-json-ts';
 
 /**
- * CoreComponent 함수에서 Props 타입을 가져옴
+ * CoreComponent 함수 (예: function BasicCard)에서 Props 타입을 가져옴
  */
-export type CoreComponentProp<CC> = CC extends (componentProps: infer P) => ChatElement ? P : never;
+export type CoreComponentFunctionProp<CC> = CC extends (componentProps: infer P) => ChatElement ? P : never;
+
+/**
+ * CoreComponent 타입 (예: type BasicCardType)에서 Props 타입을 가져옴
+ */
+export type CoreComponentTypeProp<CoreComponentType> = CoreComponentType extends ChatElement<
+  infer _ChatElementName,
+  infer ChatElementProps
+>
+  ? ChatElementProps
+  : never;
+;
+
+/**
+ * CoreComponent 타입 (예: type BasicCardType)에서 ElementName 타입을 가져옴
+ */
+export type CoreComponentTypeName<CoreComponentType> = CoreComponentType extends ChatElement<
+infer ElementName,
+infer _ChatElementProps
+>
+? ElementName
+: never;
+;
